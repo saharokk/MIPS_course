17204104bcd94c7da7afcfd3cbfa3b2716ea1d13
Dmytro <dimasakharov98@gmail.com>
2018-09-23 22:12:26 +0300

    Removed ridiculous error in mux module.

--- lab_1/mux2in1.v
+++ lab_1/mux2in1.v
@@ -6,6 +6,6 @@ input   [WIDTH-1:0]   i_dat0, i_dat1;
 input                 i_control;
 output  [WIDTH-1:0]   o_dat;
 
-assign o_dat = i_control ? i_dat0 : i_dat1;
+assign o_dat = i_control ? i_dat1 : i_dat0;
 
 endmodule
